6da4c8144d24563b9f47d9c836a213f3453da7e8
Daniil Shtobbe <daniil.shtobbe@yandex.ru>
2021-02-21 09:50:00 +0300

    Add Point2d

--- src/Point3d.java
+++ src/Point3d.java
@@ -1,6 +1,4 @@
-public class Point3d {
-    private double xCoord;
-    private double yCoord;
+public class Point3d extends Point2d {
     private double zCoord;
 
     public Point3d(double x, double y, double z) {
@@ -13,26 +11,18 @@ public class Point3d {
         this(0.0, 0.0, 0.0);
     }
 
-    public double getX(){
-        return xCoord;
-    }
-    public double getY(){
-        return yCoord;
-    }
     public double getZ(){
         return zCoord;
     }
-    public void setX(double x){
-        xCoord = x;
-    }
-    public void setY(double y){
-        yCoord = y;
-    }
+
     public void setZ(double z){
         zCoord = z;
     }
+
     public static boolean pointEqual(Point3d first, Point3d second){
-        if (first.getX() == second.getX() && first.getY() == second.getY() && first.getZ() == second.getZ()){return true;}
+        if (first.getX() == second.getX() && first.getY() == second.getY() && first.getZ() == second.getZ()){
+            return true;
+        }
         return false;
     }
     public static double distanceTo(Point3d f, Point3d s){
